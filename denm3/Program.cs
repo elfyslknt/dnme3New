8c98408fdf6025d8c323f47e5274d710d436501f
elif <deniz_yakamozu_1997@hotmail.com>
2021-01-17 16:14:48 +0300

    türkçe silindi

--- denm3/Program.cs
+++ denm3/Program.cs
@@ -7,7 +7,7 @@ namespace denm3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("merhaba dunya");
+           
             Console.ReadLine();
             Console.WriteLine("Hola Mundo");
         }
